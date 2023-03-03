21e4a590b1dc7c61d817c2cd8adc4e67e552b6ed
youssefabidi13 <youssef.abidi@etu.uae.ac.ma>
2023-03-03 15:28:19 +0100

    commit

--- Fournisseur/dashboard_fournisseur.php
+++ Fournisseur/dashboard_fournisseur.php
@@ -126,7 +126,11 @@ if (!isset($_SESSION["loggedinFournisseur"]) || $_SESSION["loggedinFournisseur"]
                                     <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
                                         Factures non payées</div>
                                     <div class="h5 mb-0 font-weight-bold text-gray-800"><?php
-                                                                                        $sql1 = 'SELECT sum(prix_TTC) as sum FROM facture  where status_f = "non_payee" and prix_HT is not NULL and prix_TTC is not null;';
+                                                                                        $sql1 = "SELECT sum(prix_TTC) as sum FROM facture f where status_f = 'non_payee' and prix_HT is not NULL and prix_TTC is not null and f.client_id in (SELECT c.ID
+                                                                                        FROM client c
+                                                                                        INNER JOIN agent a ON c.agent_id = a.id
+                                                                                        INNER JOIN manager m ON a.fournisseur_id = m.id
+                                                                                        WHERE m.id = '$id')";
                                                                                         $result1 = mysqli_query($mysqli, $sql1);
                                                                                         $row1 = $result1->fetch_assoc();
                                                                                         echo $row1['sum'] . " MAD";
@@ -146,32 +150,35 @@ if (!isset($_SESSION["loggedinFournisseur"]) || $_SESSION["loggedinFournisseur"]
                         <div class="card-body">
                             <div class="row no-gutters align-items-center">
                                 <div class="col mr-2">
-                                <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-                                        <label for="mois">Consommation par mois: </label>
+                                    <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                                        <label for="mois">Consommation par mois: </label><br>
                                         <div id="consommationM" class="h5 mb-0 font-weight-bold text-gray-800">
                                             <?php
                                             if (isset($_POST['subMois'])) {
                                                 $selectedMois = $_POST["mois"];
-                                                $sql2 = "SELECT sum(prix_TTC) as sum FROM facture  where mois='$selectedMois'";
+                                                $sql2 = "SELECT sum(prix_TTC) as sum FROM facture  where mois='$selectedMois'and client_id in (SELECT c.ID
+                                                FROM client c
+                                                INNER JOIN agent a ON c.agent_id = a.id
+                                                INNER JOIN manager m ON a.fournisseur_id = m.id
+                                                WHERE m.id = $id)";
                                                 $result2 = mysqli_query($mysqli, $sql2);
                                                 $row2 = $result2->fetch_assoc();
                                                 if ($row2['sum'] != null) {
-                                                    echo $row2['sum'] . " KWH";
+                                                    echo $row2['sum'] . " KWH <br>";
                                                 } else {
                                                     echo "<div class='alert alert-primary'>there is no data</div>";
                                                 }
-                                            } 
-                                                echo '<form method="post" action="dashboard_fournisseur.php">
+                                            }
+                                            echo '<form method="post" action="dashboard_fournisseur.php">
                                            <select id="mois" class="shadow form-control" name="mois" placeholder="mois" >';
-                                                for ($i = 1; $i < 13; $i++) {
-                                                    echo '<option value="' . $i . '">' . $i . '</option>';
-                                            
-                                                }
-                                                echo '</select><br>';
+                                            for ($i = 1; $i < 13; $i++) {
+                                                echo '<option value="' . $i . '">' . $i . '</option>';
+                                            }
+                                            echo '</select><br>';
 
-                                                echo '<button class=" btn btn-primary" type="submit" name="subMois">Submit</button>
+                                            echo '<button class=" btn btn-primary" type="submit" name="subMois">Submit</button>
                                         </form>';
-                                            
+
 
                                             ?>
                                         </div>
@@ -187,46 +194,57 @@ if (!isset($_SESSION["loggedinFournisseur"]) || $_SESSION["loggedinFournisseur"]
                     </div>
                 </div>
 
-            <!-- Earnings (Monthly) Card Example -->
-            <div class="col-xl-3 col-md-6 mb-4">
-                <div class="card border-left-info shadow h-100 py-2">
-                    <div class="card-body">
-                        <div class="row no-gutters align-items-center">
-                            <div class="col mr-2">
-                                <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Tasks
-                                </div>
-                                <div class="row no-gutters align-items-center">
-                                    <div class="col-auto">
-                                        <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">50%</div>
+                <!-- Earnings (Monthly) Card Example -->
+                <div class="col-xl-3 col-md-6 mb-4">
+                    <div class="card border-left-info shadow h-100 py-2">
+                        <div class="card-body">
+                            <div class="row no-gutters align-items-center">
+                                <div class="col mr-2">
+                                    <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Consommations par zone
                                     </div>
-                                    <div class="col">
-                                        <div class="progress progress-sm mr-2">
-                                            <div class="progress-bar bg-info" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col-auto">
+                                            <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">50%</div>
+                                        </div>
+                                        <div class="col">
+                                            <div class="progress progress-sm mr-2">
+                                                <div class="progress-bar bg-info" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div class="col-auto">
-                                <i class="fa fa-clipboard-list fa-2x text-gray-300"></i>
+                                <div class="col-auto">
+                                    <i class="fa fa-clipboard-list fa-2x text-gray-300"></i>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
 
-            <!-- Pending Requests Card Example -->
-            <div class="col-xl-3 col-md-6 mb-4">
-                <div class="card border-left-warning shadow h-100 py-2">
-                    <div class="card-body">
-                        <div class="row no-gutters align-items-center">
-                            <div class="col mr-2">
-                                <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
-                                    Pending Requests</div>
-                                <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
+                <!-- Pending Requests Card Example -->
+                <div class="col-xl-3 col-md-6 mb-4">
+                    <div class="card border-left-warning shadow h-100 py-2">
+                        <div class="card-body">
+                            <div class="row no-gutters align-items-center">
+                                <div class="col mr-2">
+                                    <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
+                                        Reclamations 
+                                        <i class="fa fa-comments fa-2x text-gray-300"></i>
+                                    </div>
+                                    <div class="h1 mb-0 font-weight-bold text-gray-800">
+                                        <?php
+                                        $sql4 = "SELECT count(*) as count FROM reclamation r where status = false and r.client_id in (SELECT c.ID
+                                                                                        FROM client c
+                                                                                        INNER JOIN agent a ON c.agent_id = a.id
+                                                                                        INNER JOIN manager m ON a.fournisseur_id = m.id
+                                                                                        WHERE m.id = '$id')";
+                                        $result4 = mysqli_query($mysqli, $sql4);
+                                        $row4 = $result4->fetch_assoc();
+                                        echo $row4['count'];
+                                        ?></div>
+                                </div>
                             </div>
-                            <div class="col-auto">
-                                <i class="fa fa-comments fa-2x text-gray-300"></i>
-                            </div>
+                            
                         </div>
                     </div>
                 </div>
